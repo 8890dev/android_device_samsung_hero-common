0d2ded608fa84c20edcfffd478ef68788e7ef090
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-27 20:46:48 +0200

    hero: Configure the board

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,1 +1,27 @@
 LOCAL_PATH := device/samsung/hero-common
+
+# Firmware
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_SLSI_VARIANT := cm
+TARGET_SOC := exynos8890
+TARGET_BOOTLOADER_BOARD_NAME := universal8890
+
+# CPU
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53.a57
+
+# Binder
+TARGET_USES_64_BIT_BINDER := true
