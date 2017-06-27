56782250ccbeacda3b9154f7b0be92a72a2423e7
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-27 21:16:06 +0200

    hero: Add offmode charger packages

--- hero-common.mk
+++ hero-common.mk
@@ -172,3 +172,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fingerprintd \
     fingerprint.exynos5
+
+# Offmode charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    cm_charger_res_images
