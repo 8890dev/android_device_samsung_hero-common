4ac10d611cef534514d2ba0db2d3c5221b1e4890
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-27 21:07:40 +0200

    hero: Import init scripts

--- hero-common.mk
+++ hero-common.mk
@@ -6,3 +6,18 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Flat device tree for boot image
 PRODUCT_PACKAGES += \
     dtbhtoolExynos
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.samsungexynos8890 \
+    init.baseband.rc \
+    init.bluetooth.rc \
+    init.gps.rc \
+    init.power.rc \
+    init.rilchip.rc \
+    init.rilcommon.rc \
+    init.samsung.rc \
+    init.samsungexynos8890.rc \
+    init.samsungexynos8890.usb.rc \
+    init.wifi.rc \
+    ueventd.samsungexynos8890.rc
