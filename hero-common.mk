adb3fc2806be4e9ba18914b2b7cb0ab236e20745
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-27 21:10:36 +0200

    hero: Add nfc packages

--- hero-common.mk
+++ hero-common.mk
@@ -111,3 +111,14 @@ PRODUCT_PACKAGES += \
 # external/wpa_supplicant_8/wpa_supplicant/wpa_supplicant_conf.mk
 PRODUCT_PACKAGES += \
     wpa_supplicant.conf
+
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
+
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    NfcNci \
+    Tag
