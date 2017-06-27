34214e0e3e86a7f495c3ecdef29d1ee55b4ef93c
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-27 21:07:08 +0200

    hero: Build fdt compiler

--- hero-common.mk
+++ hero-common.mk
@@ -2,3 +2,7 @@ LOCAL_PATH := device/samsung/hero-common
 
 # Common Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Flat device tree for boot image
+PRODUCT_PACKAGES += \
+    dtbhtoolExynos
