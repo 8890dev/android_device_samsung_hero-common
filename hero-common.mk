bb305c012bfaeb1663658f586ec94a678a30723d
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-27 21:15:16 +0200

    hero: Add Samsung HAL packages

--- hero-common.mk
+++ hero-common.mk
@@ -159,3 +159,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc \
     $(LOCAL_PATH)/configs/idc/Synaptics_RMI4_TouchPad_Sensor.idc:system/usr/idc/Synaptics_RMI4_TouchPad_Sensor.idc \
     $(LOCAL_PATH)/configs/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
+
+# Power
+PRODUCT_PACKAGES += \
+    power.universal8890
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.universal8890
